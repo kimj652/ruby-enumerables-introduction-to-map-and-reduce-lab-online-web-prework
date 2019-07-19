cd3f504e3e97b1a225394686d298eb8198e973bd
J K <kim.ju652@gmail.com>
2019-07-19 10:08:14 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -20,7 +20,8 @@ def reduce_to_total(source_array, starting_point=0)
 end
 
 def reduce_to_all_true(source_array)
-  source_array.reduce(true) { |x,y| x && y }
+  #source_array.reduce(true) { |x,y| x && y }
+  source_array.reduce(:&)
 end
 
 def reduce_to_any_true(source_array)
